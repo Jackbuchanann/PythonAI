124dbbd2c38fc806d0d94f070f51df3ff35ab678
devnumber63 <41885760+Jackbuchanann@users.noreply.github.com>
2023-11-16 01:51:59 -0500

    Update install_dependencies.ps1

--- install_dependencies.ps1
+++ install_dependencies.ps1
@@ -1,9 +1,14 @@
-# Download and install Python 3.13
-$pythonInstallerUrl = "https://www.python.org/ftp/python/3.13.0/python-3.13.0.exe"
+# Local path to Python installer
+$pythonInstallerUrl = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
 $pythonInstallerPath = "$PSScriptRoot\python_installer.exe"
 
+# Download and run the Python installer
 Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile $pythonInstallerPath
 Start-Process -Wait -FilePath $pythonInstallerPath -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1"
+
+# Verify Python installation
+$pythonPath = (Get-Command python).Source
+Write-Output "Python installed at: $pythonPath"
 
 # Install required Python packages using pip
 python -m pip install -r "$PSScriptRoot\requirements.txt"
