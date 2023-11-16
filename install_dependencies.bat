fb860e9233a768a6500f72d2fdb27240fdc1461d
devnumber63 <41885760+Jackbuchanann@users.noreply.github.com>
2023-11-16 01:49:44 -0500

    Update install_dependencies.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,10 +1,17 @@
-@echo off
+# Local path to Python installer
+$pythonInstallerUrl = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
+$pythonInstallerPath = "$PSScriptRoot\python_installer.exe"
 
-:: Set the execution policy to allow running PowerShell scripts
-powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+# Download and run the Python installer
+Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile $pythonInstallerPath
+Start-Process -Wait -FilePath $pythonInstallerPath -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1"
 
-:: Run the PowerShell script
-powershell -File "%~dp0install_dependencies.ps1"
+# Verify Python installation
+$pythonPath = (Get-Command python).Source
+Write-Output "Python installed at: $pythonPath"
 
-:: Pause to keep the console window open (optional)
-pause
+# Install required Python packages using pip
+python -m pip install -r "$PSScriptRoot\requirements.txt"
+
+# Cleanup
+Remove-Item $pythonInstallerPath
